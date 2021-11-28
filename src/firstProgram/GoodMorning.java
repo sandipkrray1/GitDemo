64fc30f75eeb62f33b2911b6c15cf568969a6fc6
sandipkrray1 <sandipkrray1@gmail.com>
2021-11-29 01:27:44 +0530

    my fist pro

---
+++ src/firstProgram/GoodMorning.java
@@ -0,0 +1,10 @@
+package firstProgram;
+
+public class GoodMorning {
+
+	public void Morning()
+	{
+		System.out.println("Good morning");
+		System.out.println("Today is sweat day!");
+	}
+}
--- src/firstProgram/Hellop.java
+++ src/firstProgram/Hellop.java
@@ -4,7 +4,9 @@ public class Hellop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GoodMorning gm = new GoodMorning();
+		gm.Morning();
+		
 	}
 
 }
